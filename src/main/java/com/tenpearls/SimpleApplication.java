d6a7b2e8e77c108cad2a4fa164da1840301fabd5
Azfar Hussain <81310231+azfarhussain-10p@users.noreply.github.com>
2025-03-12 03:15:17 +0500

    Add SimpleApplication.java

---
+++ src/main/java/com/tenpearls/SimpleApplication.java
@@ -0,0 +1,13 @@
+package com.tenpearls;
+
+/**
+ * A simple application that doesn't use Spring Boot.
+ */
+public class SimpleApplication {
+    
+    public static void main(String[] args) {
+        System.out.println("Starting SimpleApplication");
+        System.out.println("ATDAID Framework is ready for testing");
+        System.out.println("Run tests using: mvn test");
+    }
+}
