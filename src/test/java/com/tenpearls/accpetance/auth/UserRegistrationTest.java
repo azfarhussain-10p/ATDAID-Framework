7d5f0f36fe3ae115d3643936992f7c65456eb892
Azfar Hussain <81310231+azfarhussain-10p@users.noreply.github.com>
2025-03-12 03:16:00 +0500

    Add UserRegistrationTest.java

---
+++ src/test/java/com/tenpearls/accpetance/auth/UserRegistrationTest.java
@@ -0,0 +1,35 @@
+package com.tenpearls.accpetance.auth;
+
+import com.tenpearls.accpetance.BaseAcceptanceTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static io.restassured.RestAssured.*;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import io.restassured.RestAssured;
+
+public class UserRegistrationTest extends BaseAcceptanceTest {
+
+    @BeforeClass
+    @Override
+    public void setUp() {
+        super.setUp();
+        
+        // Setup test data or environment if needed
+        System.out.println("Setting up UserRegistrationTest");
+    }
+    
+    @Test(description = "Simple test to verify test setup")
+    public void simpleTest() {
+        // This is a simple test to verify that the test setup is working
+        assertThat(true).isTrue();
+    }
+    
+    @AfterClass
+    public void tearDown() {
+        // Clean up resources
+        System.out.println("Tearing down UserRegistrationTest");
+    }
+}
